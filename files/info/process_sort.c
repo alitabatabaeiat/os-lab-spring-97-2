824affb0954da13809e71390762e1fb14a4b5cd7
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-04-05 14:18:38 +0430

    add some new printk for better testing

--- files/info/process_sort.c
+++ files/info/process_sort.c
@@ -12,10 +12,12 @@ asmlinkage long sys_init_process_list(pid_t p) {
   struct task_struct* task = pid_task(pid, PIDTYPE_PID);
 
   if (task == NULL) {
+    printk("not found!");
     task = init_task;
     return 1;
   }
   plist_head->pid = task->pid;
+  printk("The pid(task->pid) is %d\n", task->pid);
   printk("The pid is %d\n", plist_head->pid);
   INIT_LIST_HEAD(plist_head->list);
 
@@ -23,10 +25,11 @@ asmlinkage long sys_init_process_list(pid_t p) {
     struct process_struct* new_plist;
     struct task_struct *child_task = list_entry(pos, struct task_struct, sibling);
     new_plist->pid = child_task->pid;
+    printk("child_task->pid: %d\n", child_task->pid);
 
     list_add(new_plist, plist_head);
     printk("plist_head->pid: %d\n", plist_head->pid);
-    printk("plist_head->next->pid: %d\n", plist_head->pid);
+    printk("plist_head->next->pid: %d\n", plist_head->next->pid);
     // __sys_init_process_list((long)task_pid_nr(child_task));
   }
 
