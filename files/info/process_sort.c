ec0951efc8552baefc5f183d84b392e86f934ba7
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-04-05 18:41:56 +0430

    loop childs ok!

--- files/info/process_sort.c
+++ files/info/process_sort.c
@@ -1,9 +1,9 @@
 #include "process_sort.h"‪
 struct process_struct* plist_head;
 
-// asmlinkage long sys_init_process_list(pid_t p‬‬, struct process_struct*) {
-//
-// }
+asmlinkage long __sys_init_process_list(pid_t p‬‬, struct process_struct*) {
+
+}
 
 asmlinkage long sys_init_process_list(pid_t p) {
   struct list_head *pos;
@@ -35,26 +35,26 @@ asmlinkage long sys_init_process_list(pid_t p) {
   INIT_LIST_HEAD(&plist_head->list);
   printk("9\n");
 
-  // list_for_each(pos, &task->children) {
-  //   printk("10\n");
-  //   struct process_struct* new_plist = (struct process_struct*)kmalloc(sizeof(struct process_struct), GFP_KERNEL);
-  //   printk("11\n");
-  //   struct task_struct *child_task = list_entry(pos, struct task_struct, sibling);
-  //   printk("12\n");
-  //   new_plist->pid = child_task->pid;
-  //   printk("13\n");
-  //   printk("child_task->pid: %d\n", child_task->pid);
-  //   printk("14\n");
-  //
-  //   list_add(new_plist->list, plist_head->list);
-  //   printk("15\n");
-  //   printk("plist_head->pid: %d\n", plist_head->pid);
-  //   struct process_struct* x = list_entry(plist_head->list->next, struct process_struct, list);
-  //   printk("x->pid: %d\n", x->pid);
-  //   // __sys_init_process_list((long)task_pid_nr(child_task));
-  //   // kfree(new_plist)
-  // }
-  // printk("16\n");
+  list_for_each(pos, &task->children) {
+    printk("10\n");
+    struct process_struct* new_plist = (struct process_struct*)kmalloc(sizeof(struct process_struct), GFP_KERNEL);
+    printk("11\n");
+    struct task_struct *child_task = list_entry(pos, struct task_struct, sibling);
+    printk("12\n");
+    new_plist->pid = child_task->pid;
+    printk("13\n");
+    printk("child_task->pid: %d\n", child_task->pid);
+    printk("14\n");
+
+    list_add(&new_plist->list, &plist_head->list);
+    printk("15\n");
+    printk("plist_head->pid: %d\n", plist_head->pid);
+    struct process_struct* x = list_entry(plist_head->list.next, struct process_struct, list);
+    printk("x->pid: %d\n", x->pid);
+    __sys_init_process_list((long)task_pid_nr(child_task));
+    // kfree(new_plist)
+  }
+  printk("16\n");
 
   return 0;
 }
