9198935c2308e7598aac4d1ce00d21faee50fda2
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-04-05 20:35:57 +0430

    main process wait until all forks done + add new fork

--- test.c
+++ test.c
@@ -3,20 +3,20 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 int main() {
+  int count = 0;
   printf("main: %d\n", getpid());
   printf("1\n");
   int pid1 = fork();
   int pid2 = fork();
-  if (pid1 == 0 && pid2 > 0) {
+  int pid3 = fork();
+
+  if (pid1 > 0 && pid2 > 0 && pid3 > 0) {
     printf("---\npid1 = %d\npid2= %d\n", pid1, pid2);
-  }
-  if (pid1 > 0 && pid2 > 0) {
-    printf("---\npid1 = %d\npid2= %d\n", pid1, pid2);
+    while(count < 1000000000) count++;
     long int amma = syscall(545, getpid());
     printf("2\n");
     printf("System call sys_init returned %ld\n", amma);
-  }
-  if (pid1 == 0 || pid2 == 0) {
+  } else {
     while(1){}
   }
   return 0;
