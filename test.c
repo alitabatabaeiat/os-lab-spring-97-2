f4e275d1666a30a840719ff7f7a9c2b9f8f20bf7
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-04-05 18:41:30 +0430

    new test

--- test.c
+++ test.c
@@ -4,8 +4,16 @@
 #include <unistd.h>
 int main() {
   printf("1\n");
-  long int amma = syscall(545, getpid());
-  printf("2\n");
-  printf("System call sys_init returned %ld\n", amma);
+  int pid1 = fork();
+  int pid2 = fork();
+  if (pid1 == 0 && pid2 > 0) {
+    printf("---\npid1 = %d\npid2= %d\n", pid1, pid2);
+  }
+  if (pid1 > 0 && pid2 > 0) {
+    printf("---\npid1 = %d\npid2= %d\n", pid1, pid2);
+    long int amma = syscall(545, getpid());
+    printf("2\n");
+    printf("System call sys_init returned %ld\n", amma);
+  }
   return 0;
 }
